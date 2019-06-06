382d499560ab2e2a0454cd452fabba34fdf2ce08
Nico Begic <nico-begic@web.de>
2019-06-06 18:14:11 +0200

    [UPDATED] Projectfiles

--- Hub World/Assets/Scripts/Placeholder.cs
+++ Hub World/Assets/Scripts/Placeholder.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Placeholder : MonoBehaviour {
+public class Placeholder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
