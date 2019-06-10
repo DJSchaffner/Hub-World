39a691b967bb603e945e27b89d8d74a62773f26b
Nico Begic <nico-begic@web.de>
2019-06-10 18:39:35 +0200

    [ADDED] player camera logic

---
+++ Hub World/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int CamSpeed;
+    public int ZoomSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(0, 0, Input.GetAxis("Mouse ScrollWheel") * ZoomSpeed * Time.deltaTime); Zoom muss durch resizen aller Gameobjekte realisiert werden
+        transform.Translate(Input.GetAxis("Horizontal") * CamSpeed * Time.deltaTime, Input.GetAxis("Vertical") * CamSpeed * Time.deltaTime, 0);
+    }
+}
