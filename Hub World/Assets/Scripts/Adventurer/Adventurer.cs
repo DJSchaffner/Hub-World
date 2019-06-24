a7f99711e6e18f7fdf5bf14a2067c6bbe51b220d
Joe K <51456979+JoeK98@users.noreply.github.com>
2019-06-24 10:34:58 +0200

    Abenteurer Klassen angelegt

---
+++ Hub World/Assets/Scripts/Adventurer/Adventurer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Adventurer
+{
+    private Food food;
+    private Drink drink;
+    private int foodPercent;
+    private int drinkPercent;
+
+    
+    public Adventurer() {
+        food = Bread;
+        drink = Beer;
+        foodPercent = 50;
+        drinkPercent = 50;
+    }
+}
---
+++ Hub World/Assets/Scripts/Adventurer/Drink.cs
@@ -0,0 +1,4 @@
+public enum Drink
+{
+    Beer, Booze, Wine
+}
