58e96fc8399f7ceea738f28b80995a08381c7cf7
DJSchaffner <jonni_soccerplayer@hotmail.de>
2019-06-26 21:00:54 +0200

    Naming convention... Changed naming of Attributes etc

--- Hub World/Assets/Scripts/Pathfinding/Graph.cs
+++ Hub World/Assets/Scripts/Pathfinding/Graph.cs
@@ -9,33 +9,33 @@ namespace Pathfinding
     {
         public class Cell
         {
-            public bool blocked     { get; set; }
-            public bool completed   { get; set; }
-            public float heuristic  { get; set; }
+            public bool IsBlocked     { get; set; }
+            public bool IsCompleted   { get; set; }
+            public float Heuristic  { get; set; }
 
-            public Cell(bool blocked, float heuristic) {
-                this.blocked = blocked;
-                this.completed = false;
-                this.heuristic = heuristic;
+            public Cell(bool isBlocked, float heuristic) {
+                this.IsBlocked = isBlocked;
+                this.IsCompleted = false;
+                this.Heuristic = heuristic;
             }
         }
 
-        public int width        { get; set; }
-        public int height       { get; set; }
-        public Vector3Int start { get; set; }
-        public Vector3Int end   { get; set; }
-        public Cell[,] cells    { get; set; }
+        public int Width        { get; set; }
+        public int Height       { get; set; }
+        public Vector3Int Start { get; set; }
+        public Vector3Int End   { get; set; }
+        private Cell[,] cells    { get; set; }
 
         public Graph(Tilemap map, Vector3Int start, Vector3Int end) {
-            this.width = map.size.x;
-            this.height = map.size.y;
-            this.start = start;
-            this.end = end;
+            this.Width = map.size.x;
+            this.Height = map.size.y;
+            this.Start = start;
+            this.End = end;
             
-            cells = new Cell[height, width];
+            cells = new Cell[Height, Width];
 
-            for (int y = 0; y < height; y++) {
-                for (int x = 0; x < width; x++) {
+            for (int y = 0; y < Height; y++) {
+                for (int x = 0; x < Width; x++) {
                     cells[y, x] = new Cell(((MapTile) map.GetTile(new Vector3Int(x, y, 0))).isBlocked, Math.Abs(end.x - x) + Math.Abs(end.y - y));
                 }
             }
@@ -46,8 +46,8 @@ namespace Pathfinding
         }
 
         public bool IsInbounds(Vector3Int position) {
-            return  position.x >= 0 && position.x < this.width && 
-                    position.y >= 0 && position.y < this.height;
+            return  position.x >= 0 && position.x < this.Width && 
+                    position.y >= 0 && position.y < this.Height;
         }
     }   
 }
