baec1cc38cf0807e7b4fea49e18fcfa97dce6685
Nico Begic <nico-begic@web.de>
2019-06-28 18:26:34 +0200

    ADDED Building-Menu structure and first design

--- Hub World/Assets/Scripts/Buildings/Tavern.cs
+++ Hub World/Assets/Scripts/Buildings/Tavern.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class Tavern : BuildingController
 {
+    public GameObject TavernMenu;
+
+    /*
+    private List<Worker> hiredWorkers;
+    private List<Worker> availableWorkers;
+    private List<Item> foodAndDrinks;
+    */
+
+
     public override void InteractWith()
     {
         Debug.Log("Opening my Tavern-Menues!");
+    }
+
+    private void showMenu()
+    {
+        TavernMenu.SetActive(true);
     }
 }
